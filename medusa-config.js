573b20ce1e353d10332b86c364bc4cc5d6543ce9
kitkatbr <91047095+kitkatbr@users.noreply.github.com>
2024-07-13 18:28:32 -0300

    up

--- medusa-config.js
+++ medusa-config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
+      storeCors: "http://localhost:8000",
+      adminCors: "http://localhost:7001",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
